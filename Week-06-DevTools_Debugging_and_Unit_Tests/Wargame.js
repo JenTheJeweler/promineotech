ccc8d278f3206177e8f8df625ed6fc2b993bbda9
Jennifer Hermann <jenmae21@outlook.com>
2023-09-09 01:15:28 -0600

    2ndcommitwargameweek06

--- Week-06-DevTools_Debugging_and_Unit_Tests/Wargame.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/Wargame.js
@@ -13,10 +13,6 @@
         Ties result in zero points for both Players.
     After all cards have been played, display the score and declare the winner.
     Write a Unit Test using Mocha and Chai for at least one of the functions you write.*/
-
-    //I've decided, to start with, that I'm going to keep all the classes in one file, for simplicity.  
-    //If it gets too difficult to read,  I may break them up later. 
-    
 /*
 OBJECTS:
 CARDS 52
@@ -24,7 +20,7 @@ CARDS 52
     suit: string
     value: number*/
     class Card {
-        constructor(suit, rank) {
+        constructor(suit, rank, value) {
             this.suit =suit
             this.rank = rank 
             this.value = value
@@ -33,7 +29,7 @@ CARDS 52
         //to return a complete card, I'll iterate through this with a loop and arrays later to create
         // the 52 cards
         createCard() {
-            return `${this.rank} of ${this.suit} and ${this.value}`;
+            return `${this.rank} of ${this.suit}`;
           }
     }
     
@@ -49,13 +45,13 @@ CARDS 52
         
         class Deck {
             constructor() {
-              this.cardStack = [];
+              this.cards = [];
               const suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades'];
               const ranks = ['Ace','2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King'];
-              const value = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+              
               for (let suit of suits) {
                 for (let rank of ranks) {
-                    for(let value of values) {
+                    let value = ranks.indexOf(rank) + 1
                   this.cards.push(new Card(suit, rank, value));
                     }
                 }
@@ -65,165 +61,84 @@ CARDS 52
     /*Actions that the Deck needs to do:
         -shuffle()
         -deal the cards (or will the players draw the cards...not sure about this*/
-        shuffle() {
-            //find a shuffle method for here---
+       
+     shuffle() {
             // Fisher-Yates Shuffle Algorithm to shuffle the cards
   for (let i = this.cards.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1)); // Generate a random index from 0 to i
-
-    // Swap the cards at indices i and j
-    [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
+    [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];// This line swaps the cards at indexes i and j randomly
   }
 }
+    Deal() { 
+        deal() {
+            return this.cards.pop(); //.pop removes and returns the last element from an array
+          }
         }
-
-        Deal() 
-        //dealing method: 
-        {const myDeck = new Deck();
-
-            const player1Card = myDeck.deal();
-            const player2Card = myDeck.deal();
-            
-            if (player1Card === null || player2Card === null) {
-              console.log("The deck is empty. Game over!");
-            } else {
-              console.log(`Player 1: ${player1Card.toString()}`);
-              console.log(`Player 2: ${player2Card.toString()}`);
-            }
-            
-         /*
-
-       */
-     /*ACTIONS:
-        √populate()
-        √Defines suits, ranks and values
-             [Heart, Ace, 1], [Spade, King, 13]
-        √Populates 52 cards
-
-        √shuffleDeck()
-        randomizes order of cards array
-        https://flaviocopes.com/how-to-shuffle-array-javascript/
-        https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    
-        //PLAYER
-    /*name: string
-    cards: []
-    score: 0*/
-    // Define Player objects
-const player1 = {
-    name: "Player 1",
-    hand: [], // An array to store the player 1's cards
-  };
+        // Function to start the game
+function start() {
+    let playGame = true; // Initialize a variable to control whether the game continues
   
-  const player2 = {
-    name: "Player 2",
-    hand: [], // An array to store the player 2's cards
-  };
+    // Use a while loop to repeatedly create and play the game until the player chooses not to play again
+    while (playGame) {
+      createGame(); // Call the function to create and play the game
+      playGame = confirm("Do you want to play again?"); // Ask the player if they want to play again and update the playGame variable accordingly
+    }
   
-  // Example: Dealing cards to the players
-  const card1 = myDeck.deal(); // Assume myDeck is your deck object with a deal() method
-  const card2 = myDeck.deal();
-  
-  if (card1 !== null && card2 !== null) {//null means if there are no more cards to deal
-    player1.hand.push(card1); // Add the dealt card to Player 1's hand
-    player2.hand.push(card2); // Add the dealt card to Player 2's hand
+    console.log("Thanks for playing!"); // Display a thank you message when the game is over
   }
   
-  // Display the players' hands
-  console.log(`${player1.name}'s hand: ${player1.hand.map(card => card.toString()).join(', ')}`);
-  console.log(`${player2.name}'s hand: ${player2.hand.map(card => card.toString()).join(', ')}`);
+  // Function to create and play the game
+  function createGame() {
+    // Create Player 1 and Player 2 objects with initial scores and empty hands
+    const player1 = {
+      name: "Player 1",
+      score: 0,
+      hand: [],
+    };
   
- 
+    const player2 = {
+      name: "Player 2",
+      score: 0,
+      hand: [],
+    };
   
-
-
-    //GAMEPLAY/DEALER
-    /*List: [] Players (2)
-    List: [] Deck
-    
-    start()
-    While loop for user to start game
-    createGame()
-    // Function to start the game
-function start() {
-  // While loop for the user to start the game
-  let playGame = true;
-
-
-    createGame()
-    Instantiate 2 Players
-    Instantiate and build Deck
-    
-    Give 26 cards to each Player
-    "If the deck of 52 cards is split between the two players, then each will only have 26 cards/turns. 
-    So instead of a "While" loop, I used a "For" loop (i=0; i<26; i++)""--Milton Aineruhanga--from Slack!
-    
-    Loop through all 26 cards and determine which value is higher. Then assign 1 point to winning card of Player 1 or Player 2
-    Display winner - higher score
-    note: could separate into multiple methods
-    */
-    function start() {
-        // While loop for the user to start the game
-        let playGame = true;
-      
-        while (playGame) {
-          createGame();
-          // Ask the user if they want to play again and set playGame accordingly
-          playGame = confirm("Do you want to play again?");
-        }
-      
-        // Game over message
-        console.log("Thanks for playing!");
-      }
-      
-      // Function to create and play the game
-      function createGame() {
-        // Instantiate 2 Players
-        const player1 = {
-          name: "Player 1",
-          score: 0,
-          hand: [],
-        };
-      
-        const player2 = {
-          name: "Player 2",
-          score: 0,
-          hand: [],
-        };
-      
-        // Instantiate and build Deck
-        const myDeck = new Deck();
-      
-        // Give 26 cards to each Player
-        for (let i = 0; i < 26; i++) {
-          const card1 = myDeck.deal();
-          const card2 = myDeck.deal();
-      
-          if (card1 !== null && card2 !== null) {
-            player1.hand.push(card1);
-            player2.hand.push(card2);
-          }
-        }
-      
-        // Loop through all 26 cards and determine which value is higher. Then assign 1 point to the winning card of Player 1 or Player 2
-        for (let i = 0; i < 26; i++) {
-          const card1 = player1.hand[i];
-          const card2 = player2.hand[i];
-      
-          if (compareCards(card1, card2) === 1) {
-            player1.score++;
-          } else if (compareCards(card1, card2) === -1) {
-            player2.score++;
-          }
-        }
-      
-        // Display the winner - higher score
-        if (player1.score > player2.score) {
-          console.log(`${player1.name} wins with a score of ${player1.score}!`);
-        } else if (player2.score > player1.score) {
-          console.log(`${player2.name} wins with a score of ${player2.score}!`);
-        } else {
-          console.log("It's a tie!");
-        }
+    const myDeck = new Deck(); // Create a new deck of cards
+  
+    // Deal 26 cards to each player
+    for (let i = 0; i < 26; i++) {
+      const card1 = myDeck.deal(); // Deal a card to Player 1
+      const card2 = myDeck.deal(); // Deal a card to Player 2
+  
+      // Check if both players received cards (the deck is not empty)
+      if (card1 !== undefined && card2 !== undefined) {
+        player1.hand.push(card1); // Add the dealt card to Player 1's hand
+        player2.hand.push(card2); // Add the dealt card to Player 2's hand
       }
     }
+  
+    // Compare the values of the cards in each player's hand to determine the winner of each round
+    for (let i = 0; i < 26; i++) {
+      const card1 = player1.hand[i]; // Get a card from Player 1's hand
+      const card2 = player2.hand[i]; // Get a card from Player 2's hand
+  
+      // Compare the values of the cards and update the scores accordingly
+      if (card1.value > card2.value) {
+        player1.score++;
+      } else if (card1.value < card2.value) {
+        player2.score++;
+      }
+    }
+  
+    // Display the winner or declare a tie based on the final scores
+    if (player1.score > player2.score) {
+      console.log(`${player1.name} wins with a score of ${player1.score}!`);
+    } else if (player2.score > player1.score) {
+      console.log(`${player2.name} wins with a score of ${player2.score}!`);
+    } else {
+      console.log("You're both winners!");
+    }
+  }
+  
+  start() 
+  // *should* start the game
+    
