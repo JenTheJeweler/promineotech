00fbf0455ab7277840309f104848822b6db33af2
Jennifer Hermann <jenmae21@outlook.com>
2023-09-15 13:01:56 -0600

    wargame improvements, runs now.

--- Week-06-DevTools_Debugging_and_Unit_Tests/Wargame.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/Wargame.js
@@ -53,10 +53,10 @@ CARDS 52
               for (let suit of suits) {
                 for (let rank of ranks) {
                 let value = ranks.indexOf(rank) + 1
-                this.cards.push(new Card(suit, rank,));
+                this.cards.push(new Card(suit, rank, value));
                     }
                 }
-            }
+            };
             
             /*Actions that the Deck needs to do:
             -shuffle()
@@ -69,98 +69,67 @@ CARDS 52
                     [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];// This line swaps the cards at 
                     //indexes i and j, randomly
                 }
-            }
+            };
             deal() {
                 if (this.cards.length === 0) {
                     return null; // Return null to indicate that the deck is empty
                 }
              return this.cards.pop(); //.pop removes and returns the last element from an array
-                   }
-        }
-        
+                }
+            };
         
         // Function to start the game
-    function start() {
-    let playGame = true; // this line initializes a variable to control whether the game continues
-  
-    // Use a while loop to repeatedly create and play the game until the player chooses not to play again
-    while (playGame) {
-      createGame(); // Call the function to create and play the game
-      playGame = confirm("Do you want to play again?"); // Ask the player if they want to play again and update the playGame variable accordingly
-    }
-    console.log("Thanks for playing!"); // Display a thank you message when the game is over
-        }
-  
         // Create Player 1 and Player 2 objects with initial scores and empty hands
         const player1 = {
             name: "Player 1",
             score: 0,
             hand: [],
-        };
+        }
         
         const player2 = {
             name: "Player 2",
             score: 0,
             hand: [],
-        };
+        }
         
         const myDeck = new Deck(); // Create a new deck of cards
-        
-        // let card1 =
-        // let card2 =  
-        // I declared these here to try to make them stop throwing
-        // undefined/null errors.  
-        //I don't know how to fix this.
-
+        myDeck.shuffle();
         // Deal 26 cards to each player
         for (let i = 0; i < 26; i++) {
-            if (myDeck.cards.length === 0) {
-                break; // Stop dealing cards if the deck is empty
-            }
-             let card1 = myDeck.cards.pop(); // Deal a card to Player 1
-             if (myDeck.cards.length === 0) {
-                break; // Stop dealing cards if the deck is empty
-            }
-             let card2 = myDeck.cards.pop(); // Deal a card to Player 2
+            let card1 = myDeck.cards.pop(); 
+            let card2 = myDeck.cards.pop(); //
             
-            //.pop
-        // Call createGame with the current players
-         createGame(player1, player2);
-            playGame = confirm("Do you want to play again?");
-                }
-            console.log("Thanks for playing!");
-        //     // Check if both players received cards (the deck is not empty)
-        //     if (card1 !== undefined && card2 !== undefined) {
-                player1.hand.push(card1); // Add the dealt card to Player 1's hand
-                player2.hand.push(card2); // Add the dealt card to Player 2's hand
-            
-        // Compare the values of the cards in each player's hand to determine the winner of each round
-        // for (let i = 0; i < 26; i++) {
-            card1 = player1.hand[i]; // Get a card from Player 1's hand
-            card2 = player2.hand[i]; // Get a card from Player 2's hand
-            
-            // Compare the values of the cards and update the scores
-            if (card1.value > card2.value) {
-                player1.score++;
-            } else if (card1.value < card2.value) {
-                player2.score++;
-            }
+            player1.hand.push(card1); // Add the dealt card to Player 1's hand
+            player2.hand.push(card2); // Add the dealt card to Player 2's hand
+        }
+        console.log(player1.hand);
+
+       
         
         // Function to create and play the game
-        function createGame() {
+        function playGame() {
             // Loop for each turn (each card dealt
+            myDeck.shuffle();
           for (let i = 0; i < 26; i++) {
-              let card1 = myDeck.deal();
-              let card2 = myDeck.deal();
+            let card1 = player1.hand.pop();
+            let card2 = player2.hand.pop();
           
-            //   if (card1 !== null && card2 !== null) {
-                  player1.hand.push(card1);
-                  player2.hand.push(card2);
-                  //this *should* print both players cards....(i don't understand why it isnt!!!)
-                console.log(`${player1.name} got ${card1.createCard()}`);
+                
+            console.log(`${player1.name} got ${card1.createCard()}`);
             console.log(`${player2.name} got ${card2.createCard()}`);
-            }}
-        // Compare the values of the cards and determine the winner
+
+            if (card1.value > card2.value){
+                player1.score+=1
+                console.log("Player 1 gets a point!");
+            } else if (card2.value >card1.value){
+                player2.score+=1
+                console.log("Player 2 gets a point!");
+            } else (card1.value==card2.value)
+                console.log("Tie, no points!");
+            }
+            };
+
+            // Compare the values of the cards and determine the winner
              for (let i = 0; i < 26; i++) {
                 let card1 = player1.hand[i];
                 let card2 = player2.hand[i];
@@ -169,17 +138,13 @@ CARDS 52
                 } else if (card1.value < card2.value) {
                      player2.score++;
                          }
-                         }
+                         };
         // Display the winner or declare a tie based on the final scores
-    if (player1.score > player2.score) {
-      console.log(`${player1.name} wins with a score of ${player1.score}!`);
-    } else if (player2.score > player1.score) {
-      console.log(`${player2.name} wins with a score of ${player2.score}!`);
-    } else {
-      console.log("Tie, no points");
-    }
- b v                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-  
-  start() 
-  // *should* start the game
-    
+            if (player1.score > player2.score) {
+              console.log(`${player1.name} wins with a score of ${player1.score}!`);
+            } else if (player2.score > player1.score) {
+              console.log(`${player2.name} wins with a score of ${player2.score}!`);
+            } else {
+            };                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+
+  playGame();
