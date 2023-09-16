d1905426a62f37be6178757eebadcd11002ace8b
Jennifer Hermann <jenmae21@outlook.com>
2023-09-16 14:30:16 -0600

    test redo

--- Week-06-DevTools_Debugging_and_Unit_Tests/Wargame/test.js
+++ Week-06-DevTools_Debugging_and_Unit_Tests/Wargame/test.js
@@ -1,15 +1,12 @@
-
-describe('Card', () => {
-  describe('createCard', () => {
+describe("Card", () => {
+  describe("createCard", () => {
     it('should return the correct card description', () => {
-      // Arrange
       const card = new Card('Hearts', 'Ace', 1);
 
-      // Act
       const cardDescription = card.createCard();
 
-      // Assert
       expect(cardDescription).to.equal('Ace of Hearts');
     });
   });
 });
+
