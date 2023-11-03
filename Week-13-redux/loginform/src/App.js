b1a7a8b8a6adddb31f5fffe2fe58f21d323053f4
Jennifer Hermann <jenmae21@outlook.com>
2023-11-02 19:19:50 -0600

    took out some unneccessaries and added border around login

--- Week-13-redux/loginform/src/App.js
+++ Week-13-redux/loginform/src/App.js
@@ -5,10 +5,12 @@ import '/node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css'; 
 
 function App() {
+  
   return (
     <div>
       <Navigation/> 
-      <button className="btn btn-primary">test</button>
+      <button className="btn btn-danger">DO NOT PUSH THIS!</button>
+
       <div className="center"> 
         <LoginForm /> 
       </div>
@@ -22,29 +24,3 @@ export default App;
 
 
 
-//Original react app styling:
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>https://legacy.reactjs.org/docs/jsx-in-depth.html#:~:text=User%2DDefined%20Components%20Must%20Be%20Capitalized&text=We%20recommend%20naming%20components%20with,before%20using%20it%20in%20JSX.
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
--- Week-13-redux/loginform/src/components/LoginForm.jsx
+++ Week-13-redux/loginform/src/components/LoginForm.jsx
@@ -6,13 +6,14 @@ import React from 'react';
 
 
 
+
 function LoginForm() {
   return (
     <div className="LoginForm">
-      <h3>Log In</h3>
+       <h3 className="border p-4">Log In</h3> {/* Add the 'border' and 'p-4' classes */}
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
-      <button  className='btn btn-primary'>Test</button>
+      <button  className='btn btn-primary'>Submit</button>
     </div>
   
   );
