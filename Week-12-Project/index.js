91273f9b7ac5911954a722087b85a2646a9e284e
Jennifer Hermann <jenmae21@outlook.com>
2023-11-03 19:07:08 -0600

    added comments to explain eveverything

--- Week-12-Project/index.js
+++ Week-12-Project/index.js
@@ -7,6 +7,7 @@
     document.getElementById("add-seed-form").addEventListener("submit", e => {
         e.preventDefault();
         //the above prevent default stops the page from reloading each time, so you can work with it
+        //Below 11-16 pulls the data from the db.json and defines it to be used in the app
         const id = document.getElementById("id").value;
         const commonName = document.getElementById("common-name").value;
         const latinName = document.getElementById("latin-name").value;
@@ -15,25 +16,25 @@
         const plantingSeason = document.getElementById("planting-season").value;
 
         // Create (POST) a new seed
-        // $(`#submitseeds`).click(function () {
             $.post(url_endpoint,{
               id: id,
               commonName: commonName,
               latinName:latinName,
               color:color,
               sunShade:$('#sun-shade').val(),
-              plantingSeason:$('#planting-season').val(), //syntactic sugar!
+              plantingSeason:$('#planting-season').val(), //syntactic sugar! both formats do the same thing,
+              // I left a couple so I could see the difference later.
             })
           })
-    // })
+   
 
-        // Read/.get - adds the new seeds data to the table
-        
+        // Read/.get - adds the new seeds data to the table.  
+        //It takes the user inputted data and appends it to the db.jsonfile
         $.get(url_endpoint).then(data => {
             data.map(seeds => {
             $('tbody').append(
               $(`
-              <tr>
+              <tr> 
                 <td> ${seeds.id}</td>
                 <td> ${seeds.commonName}</td>
                 <td> ${seeds.latinName}</td>
@@ -50,7 +51,8 @@
             )
 
     
-
+//This function updates the list with the user entered data in the Add Seed Entry/Update form
+//It took me a LONG time to get every data point to match and work correctly.  
     function updateSeedEntry() {
         let id = $("#edit-id").val();
       
@@ -66,11 +68,14 @@
           }
         })
      }
+     //This line of code below makes the update button, named in the html doc as updateseedEntry, 
+     //work, to change the entry. The id has to be an already existing id# in the list for it to change it. 
       $('#updateSeedEntry').click(updateSeedEntry)
 
       //check all data, correct it.
     // 
-    // Delete - Remove seed entry
+    // Delete - Remove seed entry. This function makes the delete button, (defined above in the .get table)
+    // named deleteSeedEntry operate:
     function deleteSeedEntry (id) {
 
         $.ajax(`${url_endpoint}/${id}`, {
@@ -79,19 +84,3 @@
       }
 
 
-    // document.getElementById("seed-list").addEventListener("click", function(e) {
-    //     if (e.target.classList.contains("delete-seed")) {
-    //         const seedId = e.target.dataset.id;
-    //         $.ajax({
-    //             url: `${url_endpoint}/${seedId}`,
-    //             method: "DELETE",
-    //             success: function() {
-    //                 refreshSeedList();
-    //             }
-    //         });
-    //     }
-    // });
-
-    // // Initial list load
-    // refreshSeedList();
-    
