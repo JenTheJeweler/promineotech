867f85a3939fba4f39c3ca860a4a0e4c95b3cfd4
Jennifer Hermann <jenmae21@outlook.com>
2023-11-30 15:54:06 -0700

    final commit wk 15

--- Week-15-REST_Fetch_Functional_Components_and_Best_Practices/houses/src/App.js
+++ Week-15-REST_Fetch_Functional_Components_and_Best_Practices/houses/src/App.js
@@ -1,170 +1,162 @@
 //
 // CRUD
-// POST 
-// GET 
-// DELETE 
+// POST
+// GET
+// DELETE
 // UPDATE
-// Using any open API of your choice you can find online, 
-// create a single page that allows for all 4 CRUD operations 
+// Using any open API of your choice you can find online,
+// create a single page that allows for all 4 CRUD operations
 // to be performed on a resource from the API.
 // Create a React component (or more, if needed) to represent the resource.
 // Make all forms and other necessary UI pieces their own components as reasonable.
 
 // ****
 
-
-
 //import all the components and react
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import AddFlowerForm from './Components/AddFlowerForm';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import AddFlowerForm from "./Components/AddFlowerForm";
 //import Flowers from './Components/Flowers';
-import FlowerCard from './Components/FlowerCard';
-
+import FlowerCard from "./Components/FlowerCard";
 
 function App() {
-  const MOCK_API_URL = "https://654c077d77200d6ba85875be.mockapi.io/WK15/Flowers"
+  const MOCK_API_URL =
+    "https://654c077d77200d6ba85875be.mockapi.io/WK15/Flowers";
   const [flowersData, setFlowersData] = useState([]);
-  //this code block sets a variable holding the API URL and 
-  //sets up an empty state (flowersData) to hold the flower information 
+  //this code block sets a variable holding the API URL and
+  //sets up an empty state (flowersData) to hold the flower information
   //obtained from the API/URL.
-console.log(flowersData)
-  useEffect(() => { // Use Effect runs once when page loads.
+  console.log(flowersData);
+  useEffect(() => {
+    // Use Effect runs once when page loads.
     fetch(MOCK_API_URL) //fetch calls api and gets data
       .then((response) => response.json()) //.json() converts the data to json
       .then((data) => setFlowersData(data)) // this updates flowersData in state.
-      .catch((error) => console.error('Error fetching flowers:', error));
+      .catch((error) => console.error("Error fetching flowers:", error));
   }, [MOCK_API_URL]);
-  // this block uses 'useEffect' to fetch data from a specified API endpoint 
-  //(MOCK_API_URL) when the component loads or whenever that URL changes. 
-  //It grabs the flower data from the (mock)API/URL and stores it in the flowersData state. 
+  // this block uses 'useEffect' to fetch data from a specified API endpoint
+  //(MOCK_API_URL) when the component loads or whenever that URL changes.
+  //It grabs the flower data from the (mock)API/URL and stores it in the flowersData state.
   //This makes sure the component has the flower information available
   // to show or work with in the app
 
-
   const addFlower = async (newFlowerData) => {
     try {
-     const response = await fetch(MOCK_API_URL, {
-      method: 'POST', //CREATE
-      headers:{
-        'Content-Type': 'application/json',
+      const response = await fetch(MOCK_API_URL, {
+        method: "POST", //CREATE
+        headers: {
+          "Content-Type": "application/json",
         },
 
         body: JSON.stringify(newFlowerData),
       });
-      //this code block creates a function that sends a request to a specific 
-      // API endpoint to add new flower data using the POST method, 
+      //this code block creates a function that sends a request to a specific
+      // API endpoint to add new flower data using the POST method,
       // and provides the data in newFlowerData.
 
-
-
-      if (response.ok) //if the response from the API goes through //
-      {
-        const data = await response.json();//- Parses the response body as JSON. 
-      //( taking a JSON-formatted string (text) and converting it into a JavaScript object)
-        setFlowersData([...flowersData, data]);// UPDATEs state with the new flower
+      if (response.ok) {
+        //if the response from the API goes through //
+        const data = await response.json(); //- Parses the response body as JSON.
+        //( taking a JSON-formatted string (text) and converting it into a JavaScript object)
+        setFlowersData([...flowersData, data]); // UPDATEs state with the new flower
       } else {
-        throw new Error('Failed to add flower');
+        throw new Error("Failed to add flower");
       }
     } catch (error) {
-      console.error('Error adding flower:', error);
+      console.error("Error adding flower:", error);
     }
   };
-   //The above code block manages the *response* after attempting to add a new flower. 
-    // if successful it updates the state with the new flower data. 
-    // If unsuccessful, it throws an error and logs the error message to the 
-    // console for debugging purposes.
+  //The above code block manages the *response* after attempting to add a new flower.
+  // if successful it updates the state with the new flower data.
+  // If unsuccessful, it throws an error and logs the error message to the
+  // console for debugging purposes.
 
-    //DELETE:
-    const deleteFlower = async (flowerId) => {
-      try {
+  //DELETE:
+  const deleteFlower = async (flowerId) => {
+    try {
+      const deleteURL = `${MOCK_API_URL}/${flowerId}`;
+      console.log("Deleting flower at:", deleteURL);
 
-        const deleteURL = `${MOCK_API_URL}/${flowerId}`;
-        console.log('Deleting flower at:', deleteURL);
-    
-        const response = await fetch(deleteURL, {
-          method: 'DELETE',  // DELETE request to remove the flower
-        });
-  
-        if (response.ok) {
-          // Filter out the deleted flower from the state
-          // const updatedFlowers = flowersData.filter((flower) => flower.id !== flowerId);
-          const updatedResponse = await fetch(MOCK_API_URL);
-          const updatedData = await updatedResponse.json();
-          setFlowersData(updatedData);
-        } else {
-          throw new Error('Failed to delete flower');
-        }
-      } catch (error) {
-        console.error('Error deleting flower:', error);
+      const response = await fetch(deleteURL, {
+        method: "DELETE", // DELETE request to remove the flower
+      });
+
+      if (response.ok) {
+        // Filter out the deleted flower from the state
+        // const updatedFlowers = flowersData.filter((flower) => flower.id !== flowerId);
+        const updatedResponse = await fetch(MOCK_API_URL);
+        const updatedData = await updatedResponse.json();
+        setFlowersData(updatedData);
+      } else {
+        throw new Error("Failed to delete flower");
       }
-    };
-      // UPDATE:
-      const updateFlower = async (flowerId, updatedFlowerData) => {
-        try {
-          const updateURL = `${MOCK_API_URL}/${flowerId}`;
-          const response = await fetch(updateURL, {
-            method: 'PUT', // Use PUT method for updating
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(updatedFlowerData),
-          });
-      
-          if (response.ok) {
-            // Fetch updated data after successful update
-            const updatedResponse = await fetch(MOCK_API_URL);
-            const updatedData = await updatedResponse.json();
-            setFlowersData(updatedData)
-            console.log(updatedData)
-          } else {
-            throw new Error('Failed to update flower');
-          }
-        } catch (error) {
-          console.error('Error updating flower:', error);
-        }
-        // This is the proper way to add the new flower to state. -MV
-       // setFlowersData([...flowersData, updatedFlowerData])
-      };
-              //  console.log(`Updating flower ${flowerId} with data:`, updatedFlowerData);
-    //};
+    } catch (error) {
+      console.error("Error deleting flower:", error);
+    }
+  };
+  // UPDATE:
+  const updateFlower = async (flowerId, updatedFlowerData) => {
+    try {
+      const updateURL = `${MOCK_API_URL}/${flowerId}`;
+      const response = await fetch(updateURL, {
+        method: "PUT", // Use PUT method for updating
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedFlowerData),
+      });
 
-  
-    return (
-      <div>
-        <h1 className="centered-header">My Garden</h1>
-        <h4 className="centered-header">Record, edit and delete your list of plants here to keep track of the growing season! 
+      if (response.ok) {
+        // Fetch updated data after successful update
+        const updatedResponse = await fetch(MOCK_API_URL);
+        const updatedData = await updatedResponse.json();
+        setFlowersData(updatedData);
+        console.log(updatedData);
+      } else {
+        throw new Error("Failed to update flower");
+      }
+    } catch (error) {
+      console.error("Error updating flower:", error);
+    }
+    // This is the proper way to add the new flower to state. -MV
+    // setFlowersData([...flowersData, updatedFlowerData])
+  };
+  //  console.log(`Updating flower ${flowerId} with data:`, updatedFlowerData);
+  //};
+
+  return (
+    <div>
+      <h1 className="centered-header">My Garden</h1>
+      <h4 className="centered-header">
+        Record, edit and delete your list of plants here to keep track of the
+        growing season!
         <br></br>
         <br></br>
-         Happy Planting!
-        </h4>
-        {/* Other components */}
-        <AddFlowerForm addFlower={addFlower} />
-        {/* Renders the AddFlowerForm component. */}
-        {/* Passes a function addFlower as a prop to the AddFlowerForm component. */}
-        {/* This allows the AddFlowerForm component to use the addFlower function 
+        Happy Planting!
+      </h4>
+      {/* Other components */}
+      <AddFlowerForm addFlower={addFlower} />
+      {/* Renders the AddFlowerForm component. */}
+      {/* Passes a function addFlower as a prop to the AddFlowerForm component. */}
+      {/* This allows the AddFlowerForm component to use the addFlower function 
             for adding new flowers. */}
-        {/* Display existing flowers */}
-        
-        <div className = 'container'>
-          {/* <div classname = 'cards-wrapper'>  */}
-          
+      {/* Display existing flowers */}
+
+      <div className="container">
+        {/* <div classname = 'cards-wrapper'>  */}
+
         {flowersData.map((flower) => (
           <FlowerCard
-          key={flower.id} 
-          flower={flower} //passes in each flower entry
-          deleteFlower={deleteFlower}
-          updateFlower={updateFlower} //passes updateFlower down as prop
-          
+            key={flower.id}
+            flower={flower} //passes in each flower entry
+            deleteFlower={deleteFlower}
+            updateFlower={updateFlower} //passes updateFlower down as prop
           />
-          ))}
-          </div>
-          </div>
-      // </div>
-      
-         
-    );
-  } 
-  
+        ))}
+      </div>
+    </div>
+    // </div>
+  );
+}
 
 export default App;
--- Week-15-REST_Fetch_Functional_Components_and_Best_Practices/houses/src/Components/FlowerCard.jsx
+++ Week-15-REST_Fetch_Functional_Components_and_Best_Practices/houses/src/Components/FlowerCard.jsx
@@ -1,110 +1,94 @@
-import React, { useState, useEffect } from 'react';
-import '../App.css';
-import UpdateFlower from './UpdateFlower';
+import React, { useState, useEffect } from "react";
+import "../App.css";
+import UpdateFlower from "./UpdateFlower";
 
 const FlowerCard = ({ flower, deleteFlower, updateFlower }) => {
-    const [isEditing, setIsEditing] = useState(false);
-    const [updatedFlower, setUpdatedFlower] = useState({ ...flower });
-     console.log(updatedFlower)
-    // Add useEffect to update the local state when the flower prop changes
-    useEffect(() => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [updatedFlower, setUpdatedFlower] = useState({ ...flower });
+  console.log(updatedFlower);
+  // Add useEffect to update the local state when the flower prop changes
+  useEffect(() => {
     setUpdatedFlower({ ...flower });
-        }, [flower]);
+  }, [flower]);
 
+  const handleDelete = async () => {
+    console.log("Deleting flower ID:", flower.id);
+    deleteFlower(flower.id); // This should trigger the deleteFlower function
+  };
 
-    const handleDelete = async () => {
-        console.log('Deleting flower ID:', flower.id);
-        deleteFlower(flower.id); // This should trigger the deleteFlower function
-    };
+  const handleEdit = () => {
+    setIsEditing(true);
+  };
 
-    const handleEdit = () => {
-        setIsEditing(true);
-      };
+  const handleCancel = () => {
+    setIsEditing(false);
+    console.log("should go away");
+    setUpdatedFlower({ ...flower }); // Reset the local state to original data on cancel
+  };
 
-      const handleCancel = () => {
-        setIsEditing(false);
-        console.log("should go away")
-        setUpdatedFlower({ ...flower }); // Reset the local state to original data on cancel
-      };
-    
-    //   const handleUpdate = async () => {
-    //     try {
-    //       const response = await updateFlower(updatedFlower.id, updatedFlower); // Send updated data to the API
-    //       if (response && response.ok) {
-    //       setIsEditing(false); // Exit edit mode after successful update
-    //       setUpdatedFlower({ ...updatedFlower });
-    //     } else {
-    //         throw new Error('Failed to update flower');
-    //       }
-    //     } catch (error) {
-    //       console.error('Error updating flower:', error);
-    //       // Handle error updating flower
-    //     }
-    //   };
+  const handleUpdate = async () => {
+    try {
+      await updateFlower(updatedFlower.id, updatedFlower); // Send updated data to the API
 
-      const handleUpdate = async () => {
-        try {
-          await updateFlower(updatedFlower.id, updatedFlower); // Send updated data to the API
-          
-          setIsEditing(false); // Exit edit mode after successful update
-          updateFlower(updatedFlower.id, updatedFlower); //Update the original flower state with the updated data
-          
-        } catch (error) {
-          console.error('Error updating flower:', error);
-          // Handle error updating flower
-        }
-      };
-    
-      const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setUpdatedFlower({
-          ...updatedFlower,
-          [name]: value,
-        });
-      };
-    
+      setIsEditing(false); // Exit edit mode after successful update
+      updateFlower(updatedFlower.id, updatedFlower); //Update the original flower state with the updated data
+    } catch (error) {
+      console.error("Error updating flower:", error);
+      // Handle error updating flower
+    }
+  };
 
-    return (
-      // <div className="container">
-      //   <div className="cards-wrapper">
-          <div className="flower-card">
-          {isEditing ?(
-            <UpdateFlower 
-            flower={updatedFlower} 
-            updateFlower={updateFlower}
-            handleCancel={handleCancel}
-            handleUpdate={handleUpdate}
-            handleInputChange={handleInputChange}
-            />
-          ) : (
-            <div>
-            <h2>{flower.commonName}</h2>
-            <p>Latin Name: {flower.latinName}</p>
-            <p>Color: {flower.color}</p>
-            <p>Sun/Shade: {flower.sunShade}</p>
-            <p>Planting Season: {flower.plantingSeason}</p>
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUpdatedFlower({
+      ...updatedFlower,
+      [name]: value,
+    });
+  };
 
-                 
-                
-    <button onClick={handleEdit}className='flower-update-button'>Update Flower</button> {/* Edit button */} 
-    <button onClick={handleDelete} className='flower-delete-button'>
-        {/* Delete button */}
-        
-            <svg xmlns="http://www.w3.org/2000/svg" 
-            width="16" 
-            height="16" 
-            fill="currentColor" 
-            className="bi bi-trash" 
-            viewBox="0 0 16 16">
-                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
-                  <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
-                 </svg>
-               </button>
-             </div>
-            )}
+  return (
+    // <div className="container">
+    //   <div className="cards-wrapper">
+    <div className="flower-card container-item">
+      {isEditing ? (
+        <UpdateFlower
+          flower={updatedFlower}
+          updateFlower={updateFlower}
+          handleCancel={handleCancel}
+          handleUpdate={handleUpdate}
+          handleInputChange={handleInputChange}
+        />
+      ) : (
+        <div>
+          <h2>{flower.commonName}</h2>
+          <p>Latin Name: {flower.latinName}</p>
+          <p>Color: {flower.color}</p>
+          <p>Sun/Shade: {flower.sunShade}</p>
+          <p>Planting Season: {flower.plantingSeason}</p>
+          <button onClick={handleEdit} className="flower-update-button">
+            Update Flower
+          </button>{" "}
+          {/* Edit button */}
+          <button onClick={handleDelete} className="flower-delete-button">
+            {/* Delete button */}
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-trash"
+              viewBox="0 0 16 16"
+            >
+              <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+              <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+            </svg>
+          </button>
         </div>
-  //     </div>
-  //  </div>
+      )}
+    </div>
+    //     </div>
+    //  </div>
   );
 };
 
